b1d7be8bec1dc875709c8f31acfb815011fe7921
linzhida <linzhida19@mails.ucas.ac.cn>
2026-02-13 01:40:38 +0800

    feat: 添加使用stdin的非递归斐波那契计算程序

---
+++ fib.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    unsigned long long a = 0, b = 1, c;
+
+    if (scanf("%d", &n) != 1) {
+        fprintf(stderr, "Error: invalid input\n");
+        return 1;
+    }
+
+    if (n < 0) {
+        fprintf(stderr, "Error: n must be non-negative\n");
+        return 1;
+    }
+
+    if (n > 93) {
+        fprintf(stderr, "Error: n must be <= 93\n");
+        return 1;
+    }
+
+    if (n == 0) {
+        printf("0\n");
+        return 0;
+    }
+
+    if (n == 1) {
+        printf("1\n");
+        return 0;
+    }
+
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    printf("%llu\n", b);
+    return 0;
+}
